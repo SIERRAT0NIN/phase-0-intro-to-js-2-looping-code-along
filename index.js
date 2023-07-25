f888232623a1c385271d4f036b56ea403c88891a
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-07-24 22:01:44 -0700

    Looping code

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event2 = "birthday";
+let cards = [];
+
+function writeCards(names, event) {
+  let cards = [];
+  names.forEach((name) => {
+    cards.push(`Thank you, ${name}, for the wonderful ${event} gift!`);
+  });
+  return cards;
+}
+writeCards(names, event2);
+console.log(cards);
+
+function countDown(number) {
+  for (let i = number; i >= 0; i--) {
+    console.log(i);
+  }
+}
+countDown(1000);
